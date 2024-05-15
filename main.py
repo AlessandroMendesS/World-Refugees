319f64db53945004698dd096d5f8d3308040ab4b
Alessandro Mendes <165946341+AlessandroMendesS@users.noreply.github.com>
2024-05-15 10:35:52 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
+import tkinter
 import customtkinter
+from PIL import ImageTk,Image
 
+customtkinter.set_appearance_mode("dark")  
+customtkinter.set_default_color_theme("green")  
 
-customtkinter.set_appearance_mode("dark")
-customtkinter.set_default_color_theme("dark-blue")
-
-def clique():
+def botao_login():
     global email, senha, ErroCerto
 
     EmailInserido = email.get()
@@ -19,64 +20,66 @@ def clique():
     if EmailInserido in Usuarios:
         if Usuarios[EmailInserido] == SenhaInserida:
             ErroCerto.configure(text="Login bem-sucedido")
-            janelaLogin.destroy()  
+            app.destroy()  
             nova_janela() 
         else:
-            ErroCerto.configure(text="Senha inválida")
+            ErroCerto.configure(text="Senha inválida", text_color="red")
     else:
         ErroCerto.configure(text="Email inválido")
 
 def nova_janela():
-    janela = customtkinter.CTk()
-    janela.geometry("500x300")
-    janela.title("Página Principal")
-
-    janela.mainloop()
-
-janelaInicial = customtkinter.CTk()
-janelaInicial.geometry('500x300')
-janelaInicial.title('Página Inicial')
-
-texto = customtkinter.CTkLabel(janelaInicial, text='Bem-Vindo', font=('Consolas',25,'bold'))
-texto.pack(padx=10, pady=30)
-
-texto1 = customtkinter.CTkLabel(janelaInicial, text='World Refugees', font=('Open Sans',24))
-texto1.pack(padx=10,pady=10)
-
-botaoAvancar = customtkinter.CTkButton(janelaInicial, text='Avançar', width=50, height=10)
-botaoAvancar.pack(padx=10,pady=10)
+    app.destroy()
+    main = customtkinter.CTk()
+    main.geometry("1280x720")
+    main.title("Página Principal")
+    main.mainloop()
 
 
-texto2 = customtkinter.CTkLabel(janelaInicial, text='From Senai', font=('Open Sans',8))
-texto2.pack(padx=10, pady=10)
+app = customtkinter.CTk()  
+app.geometry("600x440")
+app.title('Login')
 
 
-janelaInicial.mainloop()
+img1=ImageTk.PhotoImage(Image.open("image.png"))
+Plano1=customtkinter.CTkLabel(master=app,image=img1)
+Plano1.pack()
 
-    
+Caixa=customtkinter.CTkFrame(master=Plano1, width=320, height=360, corner_radius=15)
+Caixa.place(relx=0.5, rely=0.5, anchor=tkinter.CENTER)
 
-janelaLogin = customtkinter.CTk()
-janelaLogin.geometry("500x300")
-janelaLogin.title("Página de Login")
+Plano2 = customtkinter.CTkLabel(master=Caixa, text="Faça login com sua conta", font=("Century Gothic", 20))
+Plano2.place(x=35,y=45)
 
-texto = customtkinter.CTkLabel(janelaLogin, text="Fazer Login", font=("Consolas", 18, "bold"))
-texto.pack(padx=10, pady=10)
+email = customtkinter.CTkEntry(master=Caixa, placeholder_text="Email", width=220)
+email.place(x=50, y=110)
 
-email = customtkinter.CTkEntry(janelaLogin, placeholder_text="Seu email", font=("Consolas", 12))
-email.pack(padx=10,pady=10)
-
-senha = customtkinter.CTkEntry(janelaLogin, placeholder_text="Sua senha", show="*", font=("Consolas", 12))
-senha.pack(padx=10, pady=10)
-
-lembrasenha = customtkinter.CTkCheckBox(janelaLogin, text="Lembrar Login")
-lembrasenha.pack(padx=10, pady=10)
-
-botaoLogin = customtkinter.CTkButton(janelaLogin, text="Login", command=clique, fg_color='#EBB600', text_color='black')
-botaoLogin.pack(padx=10, pady=10)
-
-ErroCerto = customtkinter.CTkLabel(janelaLogin, text="")
+ErroCerto = customtkinter.CTkLabel(master=app, text="")
 ErroCerto.pack(padx=10, pady=10)
 
-janelaLogin.mainloop()
+senha = customtkinter.CTkEntry(master=Caixa, placeholder_text="Senha", width=220, show="*")
+senha.place(x=50, y=165)
 
+Plano2 = customtkinter.CTkLabel(master=Caixa, text="Esqueceu a senha?", font=("Century Gothic", 12))
+Plano2.place(x=155,y=195)
+
+Login = customtkinter.CTkButton(master=Caixa, width=220, text="Login", corner_radius=6, fg_color="#EFB810", text_color="black", command=botao_login, hover_color='#a87b05')
+Login.place(x=50, y=240)
+
+img2=customtkinter.CTkImage(Image.open("124010.png").resize((20,20)))
+img3 = customtkinter.CTkImage(Image.open("Google__G__Logo.svg.webp").resize((20,20)))
+
+
+google = customtkinter.CTkButton(master=Caixa, image=img3, text="Google", width=100, height=20, corner_radius=6, compound="left", text_color="black",
+                                fg_color="white",
+                                hover_color="#AAAAAA")
+google.place(x=50, y=290)
+
+facebook = customtkinter.CTkButton(master=Caixa, image=img2, text="Facebook", width=100, height=20, corner_radius=6, compound="left", text_color="black",
+                                fg_color="white",
+                                hover_color="#AAAAAA")
+facebook.place(x=170, y=290)
+
+
+
+app.mainloop()
 
